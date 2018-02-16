40ee0068e7d2003ab53e21674023afe8a2e2b65e
pecox <beczapeter@gmail.com>
2018-02-16 18:23:14 +0100

    a
    a

--- test1.py
+++ test1.py
@@ -4,12 +4,12 @@ Created on Fri Feb 16 18:18:06 2018
 
 @author: pbecza
 """
-
+# standart requirements
 import numpy as np
 import pandas as pd
 import glob
 import re
 from datetime import datetime as dt
 
-for i in range(5):
-    print(i)
+
+
