d2778d035fdc8fabbf2028a75fe84adbd20c0849
pecox <beczapeter@gmail.com>
2018-02-16 18:20:08 +0100

    first script
    commiting first script

---
+++ test1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 16 18:18:06 2018
+
+@author: pbecza
+"""
+
+import numpy as np
+import pandas as pd
+import glob
+import re
+from datetime import datetime as dt
+
+for i in range(5):
+    print(i)
